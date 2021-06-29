406b982e68944efd44dd0a5e2beadefcc6de1295
Naralasettithanmayee <71427794+Naralasettithanmayee@users.noreply.github.com>
2021-06-29 07:02:39 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,20 @@
+class While
+{
+    public static void main(String args[])
+    {
+        int n = 10;
+        int m = 50;
+
+        while( n > 0 && m > 0)
+        {
+            n--;
+
+            System.out.println("n = " + n);
+            m--;
+             System.out.println("m = " + m);
+
+
+
+        }
+    }
+}
