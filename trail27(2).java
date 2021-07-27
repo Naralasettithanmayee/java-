e88101ab47fa7979b77bc6f81d0375a5adb41b50
Naralasettithanmayee <71427794+Naralasettithanmayee@users.noreply.github.com>
2021-07-27 09:32:16 +0530

    Update trail27(2).java

--- trail27(2).java
+++ trail27(2).java
@@ -1,5 +1,10 @@
 class trail {
     public int trailingZeroes(int n) {
-        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);  
+        int res = 0;
+        while(n > 0){
+            n = n/5;
+            res +=n;
+        }
+        return res; 
     }
 }
